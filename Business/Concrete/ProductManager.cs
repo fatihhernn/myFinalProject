0964b590a5aaed0710e6369efdca28a5997823e7
fatih eren <fatihhernn@gmail.com>
2021-03-05 11:03:38 +0300

    have been written CacheRemoveAspect and CacheAspect attribute

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -3,6 +3,7 @@ using Business.BusinessAspects.Autofac;
 using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Businness;
@@ -39,7 +40,9 @@ namespace Business.Concrete
         //[ValidationAspect(typeof(ProductValidator))] =>attribute => metoda anlam katıyor burada add metoduna senin bir validation aspect var diyoruz onu da AUTOFAC devreye sokuyor
 
         //claim => iddia "admin", "editor" bir claim'dir.
-        [SecuredOperation("product.add,admin")]
+        //[SecuredOperation("product.add,admin")]
+
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
         {
 
@@ -99,6 +102,7 @@ namespace Business.Concrete
             return  new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.CategoryID==id));
         }
 
+        [CacheAspect]
         public IDataResult<Product> GetById(int productId)
         {
             return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductID == productId));
@@ -119,6 +123,7 @@ namespace Business.Concrete
         }
 
        // [ValidationAspect(typeof(ProductValidator))]
+       [CacheRemoveAspect("IProductService.Get")]//içerisinde get geçenleri sil
         public IResult Update(Product product)
         {
             _productDal.Update(product);
